89b2f502da9aa9b0e5a2aac748535469ee23f4a5
Kris <kristian_dilov@abv.bg>
2023-12-27 23:08:41 +0200

    handle delete

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-export const Todo = ({task}) => {
+export const Todo = ({task, deleteTodo}) => {
 
     const [checked, setChecked] = useState(false);
 
@@ -18,6 +18,7 @@ export const Todo = ({task}) => {
             />
                 {task.task}
         </label>
+        <button className='DelBtn' onClick={() => deleteTodo(task.id)}>Delete</button>
     </div>
   )
 }
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -8,18 +8,23 @@ export const TodoWrapper = () => {
     const [todos, setTodos] = useState([])
 
     const addTodo = todo => {
-        console.log(todos)
         setTodos([...todos, {id: uuidv4(), task: todo, completed: false, isEditing: false}])
-        console.log(todos)
     
+    }
+
+    const deleteTodo = id => {
+        console.log(id)
+        console.log(todos)
+        setTodos(todos.filter(todo => todo.id !== id ))
     }
 
   return (
     <div className='TodoWrapper'>
+        <h1>Get things done</h1>
         <TodoForm addTodo={addTodo}></TodoForm>
         {
         todos.map((todo, index) => ( 
-                <Todo task={todo} key={index}></Todo>
+                <Todo task={todo} key={index} deleteTodo={deleteTodo}  ></Todo>
         ))
         
         }
