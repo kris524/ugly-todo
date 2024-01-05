a61bbc481ebf48f19d42ac6cae61490a8cedd57f
Kris <kristian_dilov@abv.bg>
2024-01-05 13:28:58 +0200

    fix checkbox bug

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,7 +5,7 @@ export const Todo = ({task, deleteTodo, handleChange}) => {
   return (
     <div className='mb-4'>
         <label className='mr-4' onClick={() => handleChange(task.id)}>
-            <input className='mr-4' type="checkbox"/>
+            <input className='mr-4' type="checkbox" checked={task.completed} readOnly={true}/>
                 {task.task}
         </label>
         <button className='bg-red-800 text-white font-bold py-1 px-2 rounded-full' onClick={() => deleteTodo(task.id)}>Delete</button>
